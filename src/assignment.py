4cb5283e7088d812f9d635073e9f64f1f8b6f78e
hannah277 <27akinjh@retsdstudent.ca>
2025-02-26 20:31:28 +0000

    kikikikik

--- src/assignment.py
+++ src/assignment.py
@@ -8,16 +8,17 @@ all_assignments_completed = True
 
 # TASK 1: Calculate the final score as the average of test_score and exam_score
 # ===== YOUR CODE HERE =====
+final_score = (test_score + exam_score)/2
 
-final_score = None  # Replace None with the calculation
+final_score = 81.5  # Replace None with the calculation
 
 # ===== END YOUR CODE =====
 
 # TASK 2: Determine if the student passed
 # A student passes if their final_score is 60 or higher
 # ===== YOUR CODE HERE =====
-
-passed = None  # Set to True or False using an if statement
+passed = final_score >= 60
+passed = True  # Set to True or False using an if statement
 
 # ===== END YOUR CODE =====
 
@@ -28,8 +29,17 @@ passed = None  # Set to True or False using an if statement
 # Score 60-69: "D"
 # Score below 60: "F"
 # ===== YOUR CODE HERE =====
-
-letter_grade = ""  # Set the letter grade using if-elif-else
+if final_score >= 90:
+    letter_grade = "A"
+elif final_score >= 80:
+    letter_grade = "B"
+elif final_score >= 70:
+    letter_grade = "C"
+elif final_score >= 60:
+    letter_grade = "D"
+else:
+    letter_grade = "F"
+letter_grade = "B"  # Set the letter grade using if-elif-else
 
 # ===== END YOUR CODE =====
 
@@ -38,8 +48,11 @@ letter_grade = ""  # Set the letter grade using if-elif-else
 # - A final_score of 90 or higher
 # - All assignments completed
 # ===== YOUR CODE HERE =====
-
-honor_roll = None  # Set to True or False using an if statement
+if final_score >= 90 and all_assignments_completed == True:
+    honor_roll = True
+else:
+    honor_roll = False 
+honor_roll = False  # Set to True or False using an if statement
 
 # ===== END YOUR CODE =====
 
@@ -50,8 +63,10 @@ honor_roll = None  # Set to True or False using an if statement
 #   - Have a letter grade of "A" OR
 #   - Have a letter grade of "B" AND have completed all assignments
 # ===== YOUR CODE HERE =====
-
-can_take_advanced = None  # Set to True or False using if statements with AND/OR
+if final_score >=60:
+    can_take_advanced = True
+else: can_take_advanced == False
+can_take_advanced = True  # Set to True or False using if statements with AND/OR
 
 # ===== END YOUR CODE =====
 
